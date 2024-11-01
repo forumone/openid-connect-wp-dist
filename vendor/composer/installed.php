bd7c46e011fdd8e424ae621ab893ac42e58778e8
timnolte <timnolte@users.noreply.github.com>
2024-11-01 18:39:20 +0000

    [Create a Release Candidate] Publish from forumone/openid-connect-wp:release/artifacts/openid-connect-wp

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'forumone/openid-connect-wp',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => '905ae25d0facc9730a41dfb8cad23c6d88e21e6d',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'composer/installers' => array(
+            'pretty_version' => 'v2.3.0',
+            'version' => '2.3.0.0',
+            'reference' => '12fb2dfe5e16183de69e784a7b84046c43d97e8e',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/./installers',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'forumone/openid-connect-wp' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => '905ae25d0facc9730a41dfb8cad23c6d88e21e6d',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
